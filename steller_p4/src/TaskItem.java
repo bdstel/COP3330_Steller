0faaff80f4dd8875496c5a9cf3b7a6800fee4837
Braden <bdsteller@knights.ucf.edu>
2020-11-02 16:14:48 -0500

    Initial Commit

---
+++ steller_p4/src/TaskItem.java
@@ -0,0 +1,13 @@
+public class TaskItem
+{
+    protected String title;
+    protected String desc;
+    protected String dueDate;
+
+    public TaskItem(String title, String desc, String dueDate)
+    {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+    }
+}
---
+++ steller_p4/src/TaskList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TaskList
+{
+    protected int size;
+
+    public TaskList(ArrayList<TaskItem> items)
+    {
+
+    }
+}
